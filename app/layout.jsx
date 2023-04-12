97ad08a15eca7544953e69f1853f9ce74f6ecf21
Expensiveee <TheExpensiveee@gmail.com>
2023-04-12 14:33:44 +0000

    Metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,7 @@ const poppins = Poppins({
 export const metadata = {
   title: "Clinique Dentaire CIL",
   description: "Votre sourire c'est notre gage",
+  color: "#1e40af",
 };
 
 export default function RootLayout({ children }) {
