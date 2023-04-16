9c9864933f432a5c184a650a7583352a17127ed3
Expensiveee <TheExpensiveee@gmail.com>
2023-04-16 13:26:33 +0000

    Update page.jsx

--- app/dr/[name]/page.jsx
+++ app/dr/[name]/page.jsx
@@ -1,11 +1,24 @@
 async function getData() {
-  const res = await fetch("https://cil.expensiveee.me/api/users");
+  const res = await fetch(`${process.env.API_URL}/users`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
 
   return res.json();
+}
+
+export async function generateMetadata({ params }) {
+  const data = await getData();
+
+  const user = data[params?.name];
+
+  console.log(user);
+
+  return {
+    title: `${user?.name} - Clinique Dentaire CIL`,
+    description: `Dentiste à Casablanca, ${user?.name} ${user?.job_title}`,
+  };
 }
 
 export default async function Profile({ params }) {
