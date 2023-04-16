5e94efd6011f2fa84b54fe1a53fd7e4a7df6fb3a
Expensiveee <TheExpensiveee@gmail.com>
2023-04-16 12:46:23 +0000

    Fix

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -2,6 +2,5 @@ import { NextResponse } from "next/server";
 const data = require("@data.json");
 
 export async function GET() {
-  console.log("API CALLED");
   return NextResponse.json({ ...data });
 }
--- app/dr/[name]/page.jsx
+++ app/dr/[name]/page.jsx
@@ -1,4 +1,4 @@
-const data = require("@data.json");
+"use client";
 
 export default async function Profile({ params }) {
   const users = await fetch("http://localhost:3000/api/users")
