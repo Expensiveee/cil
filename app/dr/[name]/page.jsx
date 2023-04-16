71e87298f1054cfddbe130898172fa31539f1494
Expensiveee <TheExpensiveee@gmail.com>
2023-04-16 13:02:31 +0000

    Update page.jsx

--- app/dr/[name]/page.jsx
+++ app/dr/[name]/page.jsx
@@ -1,14 +1,9 @@
 "use client";
-const fetchData = async () => {
-  const users = await fetch("https://cil.expensiveee.me/api/users")
-    .then((res) => res.json())
-    .then((res) => res.data);
-
-  return users;
-};
 
 export default async function Profile({ params }) {
-  const users = await fetchData();
+  const data = await fetch("https://cil.expensiveee.me/api/users");
+
+  const users = data.then((res) => res.json()).then((res) => res.data);
   console.log(users);
 
   const user = users[params?.name];
