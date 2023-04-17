38e8c69adab83ca045db29d5cf5f211cfd878906
Expensiveee <TheExpensiveee@gmail.com>
2023-04-17 19:38:55 +0000

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ export default () => {
     <footer className="flex gap-32 px-4 lg:px-20 bg-blue-950 flex-col items-center justify-evenly w-full h-auto py-16">
       <div className="flex w-full gap-20 items-center justify-center lg:justify-evenly flex-col lg:flex-row">
         <div className="flex flex-col w-full text-center lg:text-left lg:w-1/4 h-auto">
-          <h5 className="text-xl font-bold text-slate-200 mb-4">L'Équipe</h5>
+          <h5 className="text-xl font-bold text-slate-200 mb-4">Équipe</h5>
           <ul className="text-md text-slate-100">
             <li>Dr Alaoui Bouhamid Ahmed - Implantologiste</li>
             <li>Dr Khairouni Hind - Pédodontiste</li>
