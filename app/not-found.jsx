162c47a1a459c639b0588a6a73db1e1081e7c684
Expensiveee <TheExpensiveee@gmail.com>
2023-08-02 00:18:27 +0100

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,3 +1,5 @@
+import Navbar from "@components/Navbar";
+
 export const metadata = {
   title: "Page pas trouvé - Clinique Dentaire CIL",
   description: "Page pas trouvé",
@@ -5,8 +7,11 @@ export const metadata = {
 
 export default function notFound() {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen h-full bg-gradient-to-r from-blue-800 to-blue-950 text-white">
-      <h1 className="text-5xl lg:text-7xl font-bold">Page pas trouvé :(</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center w-full min-h-[100vh-48px] lg:min-h-[100vh-64px] h-full bg-gradient-to-r from-blue-800 to-blue-950 text-white">
+        <h1 className="text-5xl lg:text-7xl font-bold">Page pas trouvé :(</h1>
+      </div>
+    </>
   );
 }
