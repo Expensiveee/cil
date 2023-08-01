328d171211b56904f4696589eb85374420b06615
Expensiveee <TheExpensiveee@gmail.com>
2023-08-02 00:19:36 +0100

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -9,7 +9,7 @@ export default function notFound() {
   return (
     <>
       <Navbar />
-      <div className="flex items-center justify-center w-full min-h-[100vh-48px] lg:min-h-[100vh-64px] h-full bg-gradient-to-r from-blue-800 to-blue-950 text-white">
+      <div className="flex items-center justify-center w-full h-[100vh-48px] lg:h-[100vh-64px] h-full bg-gradient-to-r from-blue-800 to-blue-950 text-white">
         <h1 className="text-5xl lg:text-7xl font-bold">Page pas trouvé :(</h1>
       </div>
     </>
